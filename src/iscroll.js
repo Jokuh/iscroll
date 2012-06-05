3d77681a9f4b3a6b5a7579df4443bc53356d5584
Zach Mathew <zmathew.to@gmail.com>
2012-06-04 21:10:19 -0400

    Fixed issue with clicks being triggered on links during scrolling if your mouse happens to remain on top of the link near the end of the scroll motion

--- src/iscroll.js
+++ src/iscroll.js
@@ -97,7 +97,9 @@ var m = Math,
 
 			// Events
 			onRefresh: null,
-			onBeforeScrollStart: function (e) { e.preventDefault(); },
+			onBeforeScrollStart: function (e) {
+				if(!hasTouch) e.preventDefault();
+			},
 			onScrollStart: null,
 			onBeforeScrollMove: null,
 			onScrollMove: null,
@@ -194,6 +196,12 @@ iScroll.prototype = {
 		this.refresh();
 	},
 	
+	_clickInterceptor: function (e) {
+		// Click event handler that is attached on the scrolled element
+		// to prevent clicks during scrolling.
+		e.preventDefault();
+	},
+
 	_scrollbar: function (dir) {
 		var that = this,
 			doc = document,
@@ -391,6 +399,10 @@ iScroll.prototype = {
 
 		if (that.options.onBeforeScrollMove) that.options.onBeforeScrollMove.call(that, e);
 
+		// Disable clicks during scrolling. We use "capture" mode for the
+		// listener to make sure it gets executed before any other handlers.
+		that.scroller.addEventListener('click', that._clickInterceptor, true);
+
 		// Zoom
 		if (that.options.zoom && hasTouch && e.touches.length > 1) {
 			c1 = m.abs(e.touches[0].pageX - e.touches[1].pageX);
@@ -479,6 +491,11 @@ iScroll.prototype = {
 		that._unbind(MOVE_EV);
 		that._unbind(END_EV);
 		that._unbind(CANCEL_EV);
+
+		// Enable clicking again
+		setTimeout(function() {  // We need to delay it 1ms or a click might get triggered
+			that.scroller.removeEventListener('click', that._clickInterceptor, true);
+		}, 1);
 
 		if (that.options.onBeforeScrollEnd) that.options.onBeforeScrollEnd.call(that, e);
 
